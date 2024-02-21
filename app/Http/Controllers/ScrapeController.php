a8dd5b4bf8397d374c21bdf5137ebdea93022316
raq0 <122687779+raq0@users.noreply.github.com>
2024-02-21 08:03:04 +0100

    added ipquality scraper and scamalytics

---
+++ app/Http/Controllers/ScrapeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Dotenv\Util\Regex;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
+
+class ScrapeController extends Controller
+{
+    public static function scamAlytics($ip)
+    {
+        $url = "https://scamalytics.com/ip/$ip";
+        $data = Http::get($url);
+        $jsonData = Str::of($data)->match('/<pre\s+style\s*=\s*"[^"]*">([\s\S]*?)<\/pre>/');
+        $json = json_decode($jsonData, true);
+        return $json;
+    }
+
+    public static function ipQualityScore($ip)
+    {
+        $apiKey = env('ipqualityscore');
+        $url = "https://ipqualityscore.com/api/json/ip/$apiKey/$ip";
+        $data = Http::get($url);
+        $json = json_decode($data, true);
+        return $json;
+    }
+
+    public static function ipData($ip)
+    {
+        $apiKey = env('ipdata');
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ScrapeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/get/{ip}', function ($ip) {
+    return ScrapeController::scamAlytics($ip);
 });
